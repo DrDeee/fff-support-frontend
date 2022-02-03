58aedd21f60aa2642f43fa93b28ab35e1d3f9207
Elias Bistry <digitalis13@gmx.de>
2022-02-03 21:47:45 +0100

    edit nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,111 +1,124 @@
-import config from "./config"
+import config from "./config";
 
 export default {
-    target: 'server',
-    ssr: false,
-    head: {
-        title: 'MessageDesk',
-        htmlAttrs: {
-            lang: 'de'
-        },
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' },
-            { name: 'format-detection', content: 'telephone=no' }
+  target: "server",
+  ssr: false,
+  head: {
+    title: "MessageDesk",
+    htmlAttrs: {
+      lang: "de",
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+
+  css: ["~/assets/scss/theme.scss"],
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: ["@nuxt/typescript-build"],
+
+  modules: [
+    "nuxt-buefy",
+    "nuxt-fontawesome",
+    "@nuxtjs/axios",
+    "@nuxtjs/auth-next",
+    "@nuxtjs/critters",
+  ],
+
+  axios: {},
+
+  buefy: {
+    materialDesignIcons: false,
+    defaultIconPack: "fas",
+    defaultIconComponent: "font-awesome-icon",
+    css: false,
+  },
+  fontawesome: {
+    imports: [
+      {
+        set: "@fortawesome/free-solid-svg-icons",
+        icons: [
+          "faBookmark",
+          "faUser",
+          "faUsers",
+          "faPlusCircle",
+          "faArrowCircleLeft",
+          "faArrowUp",
+          "faEnvelope",
+          "faLock",
+          "faSignOutAlt",
+          "faSignInAlt",
         ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
-    },
-
-    css: [
-        '~/assets/scss/theme.scss'
+      },
+      {
+        set: "@fortawesome/free-regular-svg-icons",
+        icons: ["faBookmark"],
+      },
+      {
+        set: "@fortawesome/free-brands-svg-icons",
+        icons: ["faTelegram", "faDiscord", "faWhatsapp"],
+      },
     ],
-
-    plugins: [],
-
-    components: true,
-
-    buildModules: [
-        '@nuxt/typescript-build',
-    ],
-
-    modules: [
-        'nuxt-buefy',
-        'nuxt-fontawesome',
-        '@nuxtjs/axios',
-        '@nuxtjs/auth-next',
-        '@nuxtjs/critters'
-    ],
-
-    axios: {},
-
-    buefy: {
-        materialDesignIcons: false,
-        defaultIconPack: 'fas',
-        defaultIconComponent: 'font-awesome-icon',
-        css: false
+  },
+  auth: {
+    strategies: {
+      local: false,
+      cloud: {
+        scheme: "oauth2",
+        endpoints: {
+          authorization:
+            "https://" +
+            config.keycloakInstance +
+            "/auth/realms/" +
+            config.keycloakRealm +
+            "/protocol/openid-connect/auth",
+          token:
+            "https://" +
+            config.keycloakInstance +
+            "/auth/realms/" +
+            config.keycloakRealm +
+            "/protocol/openid-connect/token",
+          userInfo:
+            "https://" +
+            config.keycloakInstance +
+            "/auth/realms/" +
+            config.keycloakRealm +
+            "/protocol/openid-connect/userinfo",
+        },
+        token: {
+          maxAge: 600,
+        },
+        responseType: "code",
+        grantType: "authorization_code",
+        clientId: config.keycloakClient,
+        scope: ["openid"],
+        codeChallengeMethod: "plain",
+      },
     },
-    fontawesome: {
-        imports: [{
-                set: '@fortawesome/free-solid-svg-icons',
-                icons: [
-                    'faBookmark',
-                    'faUser',
-                    'faUsers',
-                    'faPlusCircle',
-                    'faArrowCircleLeft',
-                    'faArrowUp',
-                    'faEnvelope',
-                    'faLock',
-                    'faSignOutAlt',
-                    'faSignInAlt'
-                ]
-            },
-            {
-                set: '@fortawesome/free-regular-svg-icons',
-                icons: ['faBookmark']
-            },
-            {
-                set: '@fortawesome/free-brands-svg-icons',
-                icons: [
-                    'faTelegram',
-                    'faDiscord',
-                    'faWhatsapp'
-                ]
-            }
-        ]
+  },
+  router: {
+    middleware: process.env.DEV_NO_AUTH === "true" ? [] : ["auth"],
+  },
+  publicRuntimeConfiguration: {
+    DEV_NO_AUTH: process.env.DEV_NO_AUTH == "true",
+    axios: {
+      browserBaseURL:
+        process.env.BROWSER_BASE_URL ||
+        process.env.BASE_URL ||
+        "http://localhost:3000",
     },
-    auth: {
-        strategies: {
-            local: false,
-            cloud: {
-                scheme: 'oauth2',
-                endpoints: {
-                    authorization: 'https://' + config.keycloakInstance + '/auth/realms/' +
-                        config.keycloakRealm + '/protocol/openid-connect/auth',
-                    token: 'https://' + config.keycloakInstance + '/auth/realms/' +
-                        config.keycloakRealm + '/protocol/openid-connect/token',
-                    userInfo: 'https://' + config.keycloakInstance + '/auth/realms/' +
-                        config.keycloakRealm + '/protocol/openid-connect/userinfo',
-                },
-                token: {
-                    maxAge: 600,
-                },
-                responseType: 'code',
-                grantType: 'authorization_code',
-                clientId: config.keycloakClient,
-                scope: ['openid'],
-                codeChallengeMethod: 'plain',
-            },
-        }
-
+  },
+  privateRuntimeConfig: {
+    axios: {
+      baseURL: process.env.BASE_URL || "http://localhost:3000",
     },
-    router: {
-        middleware: process.env.DEV_NO_AUTH === 'true' ? [] : ['auth']
-    },
-    publicRuntimeConfiguration: {
-        DEV_NO_AUTH: process.env.DEV_NO_AUTH == 'true'
-    }
-}
+  },
+};
